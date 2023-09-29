ce1af2de304bbd198c8eb6c333f23918766353dd
AlanEA <omar.encinas.cetis69@gmail.com>
2023-09-29 09:30:24 -0700

    Agregar archivos de proyecto.

---
+++ Examen_PMRI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Examen_PMRI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
